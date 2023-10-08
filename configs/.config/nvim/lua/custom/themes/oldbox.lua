ee786b9dd92ec7b0d66324d78df893353b292412
Christopher Smith <49736737+yeehawtoast@users.noreply.github.com>
2023-10-08 13:47:13 -0500

    Add files via upload

---
+++ configs/.config/nvim/lua/custom/themes/oldbox.lua
@@ -0,0 +1,57 @@
+local M = {}
+
+M.base_30 = {
+  white = "#585148",
+  darker_black = "#e8dbb2",
+  black = "#e9e2c9", --  nvim bg
+  black2 = "#e3d6ad",
+  one_bg = "#e5d8af", --
+  one_bg2 = "#d8cba2",
+  one_bg3 = "#cabd94",
+  grey = "#c0b38a",
+  grey_fg = "#b6a980",
+  grey_fg2 = "#ac9f76",
+  light_grey = "#a2956c",
+  red = "#7a1405",
+  baby_pink = "#85208a",
+  pink = "#683504",
+  line = "#ded1a8", -- for lines like vertsplit
+  green = "#50620f",
+  vibrant_green = "#333f0a",
+  nord_blue = "#2a5173",
+  blue = "#366994",
+  yellow = "#957c16",
+  sun = "#B4961a",
+  purple = "#513e56",
+  dark_purple = "#674f6e",
+  teal = "#0e616a",
+  orange = "#984e06",
+  cyan = "#158f9c",
+  statusline_bg = "#e9dcb3",
+  lightbg = "#ddd0a7",
+  pmenu_bg = "#158f9c",
+  folder_bg = "#583a24",
+}
+
+M.base_16 = {
+  base00 = "#e9e2c9",
+  base01 = "#e3d6ad",
+  base02 = "#e5d8af",
+  base03 = "#d8cba2", 
+  base04 = "#583a24",
+  base05 = "#585148",
+  base06 = "#413d37",
+  base07 = "#e9e1c3",
+  base08 = "#7a1405",
+  base09 = "#50620f",
+  base0A = "#b4961a",
+  base0B = "#366994",
+  base0C = "#674f6e",
+  base0D = "#158f9c",
+  base0E = "#984e06",
+  base0F = "#85308a",
+}
+
+M.type = "dark"
+
+return M
